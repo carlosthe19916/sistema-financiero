a3f14c0b39a7ffcf2108dd81966a7c677247cff7
carlosthe19916@gmail.com <eA4QU8hx6PE2>
2013-09-02 12:18:37 -0500

    dao

---
+++ SistemaFinanciero/SistemaFinanciero-ejb/src/main/java/org/ventura/model/AccionistaPK.java
@@ -0,0 +1,57 @@
+package org.ventura.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * The primary key class for the accionista database table.
+ * 
+ */
+@Embeddable
+public class AccionistaPK implements Serializable {
+	//default serial version id, required for serializable classes.
+	private static final long serialVersionUID = 1L;
+
+	@Column(insertable=false, updatable=false)
+	private String ruc;
+
+	@Column(insertable=false, updatable=false)
+	private String dni;
+
+	public AccionistaPK() {
+	}
+	public String getRuc() {
+		return this.ruc;
+	}
+	public void setRuc(String ruc) {
+		this.ruc = ruc;
+	}
+	public String getDni() {
+		return this.dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public boolean equals(Object other) {
+		if (this == other) {
+			return true;
+		}
+		if (!(other instanceof AccionistaPK)) {
+			return false;
+		}
+		AccionistaPK castOther = (AccionistaPK)other;
+		return 
+			this.ruc.equals(castOther.ruc)
+			&& this.dni.equals(castOther.dni);
+	}
+
+	public int hashCode() {
+		final int prime = 31;
+		int hash = 17;
+		hash = hash * prime + this.ruc.hashCode();
+		hash = hash * prime + this.dni.hashCode();
+		
+		return hash;
+	}
+}
---
+++ SistemaFinanciero/SistemaFinanciero-ejb/src/main/java/org/ventura/model/ChequePK.java
@@ -0,0 +1,56 @@
+package org.ventura.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * The primary key class for the cheque database table.
+ * 
+ */
+@Embeddable
+public class ChequePK implements Serializable {
+	//default serial version id, required for serializable classes.
+	private static final long serialVersionUID = 1L;
+
+	@Column(insertable=false, updatable=false)
+	private Integer numerochequeraporcliente;
+
+	private Integer numerocheque;
+
+	public ChequePK() {
+	}
+	public Integer getNumerochequeraporcliente() {
+		return this.numerochequeraporcliente;
+	}
+	public void setNumerochequeraporcliente(Integer numerochequeraporcliente) {
+		this.numerochequeraporcliente = numerochequeraporcliente;
+	}
+	public Integer getNumerocheque() {
+		return this.numerocheque;
+	}
+	public void setNumerocheque(Integer numerocheque) {
+		this.numerocheque = numerocheque;
+	}
+
+	public boolean equals(Object other) {
+		if (this == other) {
+			return true;
+		}
+		if (!(other instanceof ChequePK)) {
+			return false;
+		}
+		ChequePK castOther = (ChequePK)other;
+		return 
+			this.numerochequeraporcliente.equals(castOther.numerochequeraporcliente)
+			&& this.numerocheque.equals(castOther.numerocheque);
+	}
+
+	public int hashCode() {
+		final int prime = 31;
+		int hash = 17;
+		hash = hash * prime + this.numerochequeraporcliente.hashCode();
+		hash = hash * prime + this.numerocheque.hashCode();
+		
+		return hash;
+	}
+}
