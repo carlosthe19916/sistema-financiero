11009fbfef19ebe5673693ed36418a9b812dd9d2
Javier <Javier@Javier-PC>
2013-09-09 13:07:56 -0500

    12

--- SistemaFinanciero/SistemaFinanciero-web/src/main/java/org/venturabank/managedbean/PersonaJuridicaMB.java
+++ SistemaFinanciero/SistemaFinanciero-web/src/main/java/org/venturabank/managedbean/PersonaJuridicaMB.java
@@ -1,31 +1,68 @@
 package org.venturabank.managedbean;
 
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.List;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.NoneScoped;
 
+import org.ventura.facade.PersonajuridicaFacadeLocal;
+import org.ventura.facade.TipoempresaFacadeLocal;
 import org.ventura.model.Personajuridica;
+import org.ventura.model.Tipoempresa;
 
 @ManagedBean
 @NoneScoped
-public class PersonaJuridicaMB implements Serializable{
+public class PersonaJuridicaMB implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	private Personajuridica oPersonajuridica;
 
-	public PersonaJuridicaMB(){
-		oPersonajuridica = new Personajuridica();
-		
+	private Personajuridica oPersonajuridica;
+	@EJB
+	TipoempresaFacadeLocal tipoEmpresaFacadeLocal;
+	@EJB
+	PersonajuridicaFacadeLocal personaJuridicaFacadeLocal;
+	private List<Tipoempresa> listaTipoempresas;
+	private Tipoempresa otipoempresaSeleccionada;
+
+	public PersonaJuridicaMB() {
+		this.oPersonajuridica = new Personajuridica();
+		this.otipoempresaSeleccionada = new Tipoempresa();
 	}
+
+	
+	public void insertarPersonaJuridica(){
+		personaJuridicaFacadeLocal.create(oPersonajuridica);
+	}
+	
 	public Personajuridica getoPersonajuridica() {
 		return oPersonajuridica;
 	}
 
 	public void setoPersonajuridica(Personajuridica oPersonajuridica) {
 		this.oPersonajuridica = oPersonajuridica;
+	}
+
+	public List<Tipoempresa> getListaTipoempresas() {
+		initListaTipoempresas();
+		return listaTipoempresas;
+	}
+
+	public void setListaTipoempresas(List<Tipoempresa> listaTipoempresas) {
+		this.listaTipoempresas = listaTipoempresas;
+	}
+
+	public Tipoempresa getTipoempresaSeleccionada() {
+		return otipoempresaSeleccionada;
+	}
+
+	public void setTipoempresaSeleccionada(Tipoempresa tipoempresaSeleccionada) {
+		this.otipoempresaSeleccionada = tipoempresaSeleccionada;
+	}
+	
+	public void initListaTipoempresas() {
+		listaTipoempresas = (List<Tipoempresa>) tipoEmpresaFacadeLocal.findAll();
 	}
 
 }
