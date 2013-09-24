b78e0dc53635dea95fa24a11cc6f8200135afaae
ripley <ripley@FLORES>
2013-09-24 13:32:40 -0500

    busqueda

--- SistemaFinanciero/SistemaFinanciero-ejb/src/main/java/org/ventura/entity/Personanaturalcliente.java
+++ SistemaFinanciero/SistemaFinanciero-ejb/src/main/java/org/ventura/entity/Personanaturalcliente.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.*;
 
+
 /**
  * The persistent class for the personanaturalcliente database table.
  * 
@@ -12,9 +13,13 @@ import javax.persistence.*;
 @Entity
 @Table(name = "personanaturalcliente", schema = "cliente")
 @NamedQuery(name = "Personanaturalcliente.findAll", query = "SELECT p FROM Personanaturalcliente p")
+
+@NamedQueries({@NamedQuery(name = Personanaturalcliente.ALL, query = "Select s From Personanaturalcliente s")})
 public class Personanaturalcliente implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+	
+	public final static String ALL = "org.ventura.model.Personanaturalcliente.ALL"; 
+	
 	@Id
 	@Column(unique = true, nullable = false, length = 8)
 	private String dni;
