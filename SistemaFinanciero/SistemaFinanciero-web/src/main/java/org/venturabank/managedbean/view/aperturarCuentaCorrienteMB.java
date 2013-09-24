edc1f1dea55e2e7cce94f8465916282d19a01e0c
carlosthe19916@gmail.com <eA4QU8hx6PE2>
2013-09-24 11:53:09 -0500

    3

---
+++ SistemaFinanciero/SistemaFinanciero-web/src/main/java/org/venturabank/managedbean/view/aperturarCuentaCorrienteMB.java
@@ -0,0 +1,116 @@
+package org.venturabank.managedbean.view;
+
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+import org.ventura.boundary.local.CuentaahorroServiceLocal;
+import org.ventura.boundary.local.CuentacorrienteServiceLocal;
+import org.ventura.entity.Beneficiariocuenta;
+import org.ventura.entity.Cuentaahorro;
+import org.ventura.entity.Cuentaahorrohistorial;
+import org.ventura.entity.Cuentacorriente;
+import org.ventura.entity.Personajuridica;
+import org.ventura.entity.Personanatural;
+import org.ventura.entity.Personanaturalcliente;
+import org.ventura.entity.Titularcuenta;
+import org.venturabank.managedbean.BeneficiariosMB;
+import org.venturabank.managedbean.DatosFinancierosCuentaAhorroMB;
+import org.venturabank.managedbean.PersonaJuridicaMB;
+import org.venturabank.managedbean.PersonaNaturalMB;
+import org.venturabank.managedbean.TitularesMB;
+import org.venturabank.util.ComboMB;
+
+@ManagedBean
+@ViewScoped
+public class aperturarCuentaCorrienteMB implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@EJB
+	private CuentacorrienteServiceLocal cuentacorrienteServiceLocal;
+
+	private Cuentacorriente cuentacorriente;
+
+	@ManagedProperty(value = "#{comboMB}")
+	private ComboMB<String> comboTipoPersona;
+
+	@ManagedProperty(value = "#{personaNaturalMB}")
+	private PersonaNaturalMB personaNaturalMB;
+
+	/**
+	 * 
+	 * CONTRUCT POSTCONTRUC PREDESTROY
+	 * 
+	 **/
+
+	public aperturarCuentaCorrienteMB() {
+		this.cuentacorriente = new Cuentacorriente();
+	}
+
+	@PostConstruct
+	private void initValues() {
+
+		this.cargarCombos();
+
+	}
+
+	/**
+	 * 
+	 * BUSSINES LOGIC
+	 * 
+	 * **/
+
+	public void cargarCombos() {
+		comboTipoPersona.getItems().put(1, "Persona Natural");
+		comboTipoPersona.getItems().put(2, "Persona Juridica");
+		comboTipoPersona.setItemSelected(1);
+	}
+
+	public void createCuentaCorriente() {
+		this.cuentacorrienteServiceLocal.create(cuentacorriente);
+	}
+
+	public boolean isPersonaNatural() {
+		if (comboTipoPersona.getItemSelected() == 1)
+			return true;
+		else
+			return false;
+	}
+
+	public boolean isPersonaJuridica() {
+		if (comboTipoPersona.getItemSelected() == 2)
+			return true;
+		else
+			return false;
+	}
+
+	/**
+	 * 
+	 * GETTER AND SETTER
+	 * 
+	 * **/
+
+	public ComboMB<String> getComboTipoPersona() {
+		return comboTipoPersona;
+	}
+
+	public void setComboTipoPersona(ComboMB<String> comboTipoPersona) {
+		this.comboTipoPersona = comboTipoPersona;
+	}
+
+	public PersonaNaturalMB getPersonaNaturalMB() {
+		return personaNaturalMB;
+	}
+
+	public void setPersonaNaturalMB(PersonaNaturalMB personaNaturalMB) {
+		this.personaNaturalMB = personaNaturalMB;
+	}
+
+}
---
+++ SistemaFinanciero/SistemaFinanciero-web/src/main/java/org/venturabank/managedbean/view/aperturarCuentaPlazoFijoMB.java
@@ -0,0 +1,118 @@
+package org.venturabank.managedbean.view;
+
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+import org.ventura.boundary.local.CuentaahorroServiceLocal;
+import org.ventura.boundary.local.CuentaplazofijoServiceLocal;
+import org.ventura.entity.Beneficiariocuenta;
+import org.ventura.entity.Cuentaahorro;
+import org.ventura.entity.Cuentaahorrohistorial;
+import org.ventura.entity.Cuentaplazofijo;
+import org.ventura.entity.Personajuridica;
+import org.ventura.entity.Personajuridicacliente;
+import org.ventura.entity.Personanatural;
+import org.ventura.entity.Personanaturalcliente;
+import org.ventura.entity.Titularcuenta;
+import org.venturabank.managedbean.BeneficiariosMB;
+import org.venturabank.managedbean.DatosFinancierosCuentaAhorroMB;
+import org.venturabank.managedbean.PersonaJuridicaMB;
+import org.venturabank.managedbean.PersonaNaturalMB;
+import org.venturabank.managedbean.TitularesMB;
+import org.venturabank.util.ComboMB;
+
+@ManagedBean
+@ViewScoped
+public class aperturarCuentaPlazoFijoMB implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@EJB
+	private CuentaplazofijoServiceLocal cuentaaplazofijoServiceLocal;
+
+	private Cuentaplazofijo cuentaplazofijo;
+
+	@ManagedProperty(value = "#{comboMB}")
+	private ComboMB<String> comboTipoPersona;
+
+	@ManagedProperty(value = "#{personaNaturalMB}")
+	private PersonaNaturalMB personaNaturalMB;
+
+	/**
+	 * 
+	 * CONTRUCT POSTCONTRUC PREDESTROY
+	 * 
+	 **/
+
+	public aperturarCuentaPlazoFijoMB() {
+		this.cuentaplazofijo = new Cuentaplazofijo();
+	}
+
+	@PostConstruct
+	private void initValues() {
+
+		this.cargarCombos();
+	}
+
+	/**
+	 * 
+	 * BUSSINES LOGIC
+	 * 
+	 * **/
+
+	public void cargarCombos(){
+		comboTipoPersona.getItems().put(1, "Persona Natural");
+		comboTipoPersona.getItems().put(2, "Persona Juridica");
+		comboTipoPersona.setItemSelected(1);
+	}
+	
+	public void createCuentaPlazoFijo() {
+	
+		this.cuentaaplazofijoServiceLocal.create(cuentaplazofijo);				
+	}
+
+	public boolean isPersonaNatural() {
+		if (comboTipoPersona.getItemSelected() == 1)
+			return true;
+		else
+			return false;
+	}
+
+	public boolean isPersonaJuridica() {
+		if (comboTipoPersona.getItemSelected() == 2)
+			return true;
+		else
+			return false;
+	}
+
+	/**
+	 * 
+	 * GETTER AND SETTER
+	 * 
+	 * **/
+
+	public ComboMB<String> getComboTipoPersona() {
+		return comboTipoPersona;
+	}
+
+	public void setComboTipoPersona(ComboMB<String> comboTipoPersona) {
+		this.comboTipoPersona = comboTipoPersona;
+	}
+
+	public PersonaNaturalMB getPersonaNaturalMB() {
+		return personaNaturalMB;
+	}
+
+	public void setPersonaNaturalMB(PersonaNaturalMB personaNaturalMB) {
+		this.personaNaturalMB = personaNaturalMB;
+	}
+
+
+}
