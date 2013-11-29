ef08b90abbe45787d6f74c9f0b6b1ec717dc852b
carlosthe19916@gmail.com <eA4QU8hx6PE2>
2013-11-29 17:25:27 -0500

    ca

--- SistemaFinanciero/SistemaFinanciero-ejb/src/main/java/org/ventura/control/CajaServiveBean.java
+++ SistemaFinanciero/SistemaFinanciero-ejb/src/main/java/org/ventura/control/CajaServiveBean.java
@@ -1,8 +1,21 @@
 package org.ventura.control;
 
+import javax.ejb.Local;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.inject.Named;
+
 import org.ventura.boundary.local.CajaServiceLocal;
+import org.ventura.boundary.remote.CajaServiceRemote;
 import org.ventura.entity.schema.caja.Caja;
 
+@Named
+@Stateless
+@Local(CajaServiceLocal.class)
+@Remote(CajaServiceRemote.class)
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
 public class CajaServiveBean implements CajaServiceLocal{
 
 	public CajaServiveBean() {
