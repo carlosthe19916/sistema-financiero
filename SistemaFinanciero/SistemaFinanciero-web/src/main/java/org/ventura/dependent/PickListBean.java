c0f1c5b04a5fa0b11f623087679de1138a64f898
Jhon <Jhon@Flores>
2013-12-02 17:43:41 -0500

    133

--- SistemaFinanciero/SistemaFinanciero-web/src/main/java/org/ventura/dependent/PickListBean.java
+++ SistemaFinanciero/SistemaFinanciero-web/src/main/java/org/ventura/dependent/PickListBean.java
@@ -25,22 +25,37 @@ public class PickListBean<E> {
 	private DualListModel<E> result;
 	private List<E> source;
 	private List<E> target;
+	
 
 	public PickListBean() {
-            this.source = new ArrayList<E>();
-            this.target = new ArrayList<E>();
-            result = new DualListModel<E>(source, target);
+            this.setSource(new ArrayList<E>());
+            this.setTarget(new ArrayList<E>());
+            result = new DualListModel<E>(getSource(), getTarget());
     }
 	
 	public void initValuesFromNamedQueryName(String namedQueryName) {
 		List<E> list = crudService.findWithNamedQuery(namedQueryName);
-		this.result = (DualListModel<E>) list;
+		this.source = (List<E>) list;
 	}
 
 	public void initValuesFromNamedQueryName(String namedQueryName, Map<String, Object> parameters) {
 		List<E> list = crudService.findWithNamedQuery(namedQueryName, parameters);
-		this.result = (DualListModel<E>) list;
+		this.source = (List<E>) list;
 	}
+	
+	public void onTransfer(TransferEvent event) {  
+        StringBuilder builder = new StringBuilder();  
+        for(Object item : event.getItems()) {  
+            builder.append(item).append("<br />");  
+        }  
+          
+        FacesMessage msg = new FacesMessage();  
+        msg.setSeverity(FacesMessage.SEVERITY_INFO);  
+        msg.setSummary("Boveda Transferida");  
+        msg.setDetail(builder.toString());  
+          
+        FacesContext.getCurrentInstance().addMessage(null, msg);  
+    }
 
 	public DualListModel<E> getResult() {
 		return result;
@@ -48,5 +63,21 @@ public class PickListBean<E> {
 	
 	public void setResult(DualListModel<E> result) {
 		this.result = result;
+	}
+
+	public List<E> getSource() {
+		return source;
+	}
+
+	public void setSource(List<E> source) {
+		this.source = source;
+	}
+
+	public List<E> getTarget() {
+		return target;
+	}
+
+	public void setTarget(List<E> target) {
+		this.target = target;
 	}
 }
