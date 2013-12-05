ebfb73a410fac3792736dacb3f4dca6eedb461a2
carlosthe19916@gmail.com <eA4QU8hx6PE2>
2013-12-05 16:29:28 -0500

    a

--- SistemaFinanciero/SistemaFinanciero-web/src/main/java/org/ventura/caja/view/TransaccionCuentabancariaCajaBean.java
+++ SistemaFinanciero/SistemaFinanciero-web/src/main/java/org/ventura/caja/view/TransaccionCuentabancariaCajaBean.java
@@ -1,6 +1,7 @@
 package org.ventura.caja.view;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -11,11 +12,12 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.ventura.boundary.local.DenominacionmonedaServiceLocal;
+import org.ventura.caja.dependent.BuscarCuentabancariaBean;
 import org.ventura.dependent.CalculadoraBean;
 import org.ventura.dependent.ComboBean;
 import org.ventura.entity.schema.caja.Denominacionmoneda;
+import org.ventura.entity.schema.caja.Moneda;
 import org.ventura.entity.schema.caja.Tipotransaccion;
-import org.ventura.entity.schema.caja.Transaccioncaja;
 import org.ventura.entity.schema.maestro.Tipomoneda;
 
 @Named
@@ -28,23 +30,34 @@ public class TransaccionCuentabancariaCajaBean implements Serializable {
 	private DenominacionmonedaServiceLocal denominacionmonedaServiceLocal;
 
 	@Inject
-	private Transaccioncaja transaccioncaja;
-	
-	@Inject
 	private ComboBean<Tipotransaccion> comboTipotransaccion;
 	@Inject
 	private ComboBean<Tipomoneda> comboTipomoneda;
 	@Inject
 	private CalculadoraBean calculadoraBean;
 
+	//Datos pagina principal
+	@Inject
+	private Tipotransaccion tipotransaccion;	
+	@Inject
+	private Tipomoneda tipomoneda;
+	@Inject
+	private Moneda monto;
+	private String numeroCuenta;
+	private String referencia;
+
+	@Inject
+	private BuscarCuentabancariaBean buscarCuentabancariaBean;
+	
 	public TransaccionCuentabancariaCajaBean() {
-		// TODO Auto-generated constructor stub
+
 	}
 
 	@PostConstruct
 	private void initialize() {
 		try {
-			comboTipotransaccion.initValuesFromNamedQueryName(Tipotransaccion.ALL_ACTIVE);
+			comboTipotransaccion
+					.initValuesFromNamedQueryName(Tipotransaccion.ALL_ACTIVE);
 			comboTipomoneda.initValuesFromNamedQueryName(Tipomoneda.ALL_ACTIVE);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -55,9 +68,14 @@ public class TransaccionCuentabancariaCajaBean implements Serializable {
 	public void loadDenominacionmonedaCalculadora() {
 		List<Denominacionmoneda> list;
 		try {
-			Tipomoneda tipmoneda = new Tipomoneda();
-			//list = denominacionmonedaServiceLocal.getDenominacionmonedasActive(tipomoneda);
-			//calculadoraBean.setDenominaciones(list);
+			Tipomoneda tipomoneda = this.tipomoneda;
+			if (tipomoneda != null) {
+				list = denominacionmonedaServiceLocal
+						.getDenominacionmonedasActive(tipomoneda);
+			} else {
+				list = new ArrayList<Denominacionmoneda>();
+			}
+			calculadoraBean.setDenominaciones(list);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -66,15 +84,19 @@ public class TransaccionCuentabancariaCajaBean implements Serializable {
 
 	public void changeTipotransaccion(ValueChangeEvent event) {
 		Integer key = (Integer) event.getNewValue();
-		Tipotransaccion tipotransaccionSelected = comboTipotransaccion.getObjectItemSelected(key);		
-		transaccioncaja.setTipotransaccion(tipotransaccionSelected);
+		Tipotransaccion tipotransaccionSelected = comboTipotransaccion
+				.getObjectItemSelected(key);
+		this.tipotransaccion = tipotransaccionSelected;
 	}
-	
+
 	public void changeTipomoneda(ValueChangeEvent event) {
 		Integer key = (Integer) event.getNewValue();
-		Tipomoneda tipomonedaSelected = comboTipomoneda.getObjectItemSelected(key);	
+		Tipomoneda tipomonedaSelected = comboTipomoneda.getObjectItemSelected(key);
+		this.tipomoneda = tipomonedaSelected;
+		this.monto = new Moneda();
+		loadDenominacionmonedaCalculadora();
 	}
-	
+
 	public DenominacionmonedaServiceLocal getDenominacionmonedaServiceLocal() {
 		return denominacionmonedaServiceLocal;
 	}
@@ -107,5 +129,60 @@ public class TransaccionCuentabancariaCajaBean implements Serializable {
 
 	public void setCalculadoraBean(CalculadoraBean calculadoraBean) {
 		this.calculadoraBean = calculadoraBean;
+	}
+
+	public Tipotransaccion getTipotransaccion() {
+		return tipotransaccion;
+	}
+
+	public void setTipotransaccion(Tipotransaccion tipotransaccion) {
+		this.tipotransaccion = tipotransaccion;
+	}
+
+	public String getNumeroCuenta() {
+		return numeroCuenta;
+	}
+
+	public void setNumeroCuenta(String numeroCuenta) {
+		this.numeroCuenta = numeroCuenta;
+	}
+
+	public Tipomoneda getTipomoneda() {
+		return tipomoneda;
+	}
+
+	public void setTipomoneda(Tipomoneda tipomoneda) {
+		this.tipomoneda = tipomoneda;
+	}
+
+	public Moneda getMonto() {
+		return monto;
+	}
+
+	public void setMonto(Moneda monto) {
+		Moneda result = new Moneda(monto);
+		this.monto = result;
+	}
+
+	public void setMontoFromCalculadora() {
+		Moneda result = this.calculadoraBean.getTotal();
+		this.monto = result;
+	}
+
+	public String getReferencia() {
+		return referencia;
+	}
+
+	public void setReferencia(String referencia) {
+		this.referencia = referencia;
+	}
+
+	public BuscarCuentabancariaBean getBuscarCuentabancariaBean() {
+		return buscarCuentabancariaBean;
+	}
+
+	public void setBuscarCuentabancariaBean(
+			BuscarCuentabancariaBean buscarCuentabancariaBean) {
+		this.buscarCuentabancariaBean = buscarCuentabancariaBean;
 	}
 }
---
+++ SistemaFinanciero/SistemaFinanciero-web/src/main/java/org/ventura/dependent/CalculadoraRowBean.java
@@ -0,0 +1,52 @@
+package org.ventura.dependent;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Named;
+
+import org.ventura.entity.schema.caja.Denominacionmoneda;
+import org.ventura.entity.schema.caja.Moneda;
+
+@Named
+@Dependent
+public class CalculadoraRowBean {
+
+	private Denominacionmoneda denominacionmoneda;
+	private Integer cantidad;
+
+	public CalculadoraRowBean() {
+		this.cantidad = 0;
+	}
+
+	public CalculadoraRowBean(Denominacionmoneda denominacionmoneda) {
+		this.denominacionmoneda = denominacionmoneda;
+		this.cantidad = 0;
+	}
+
+	public CalculadoraRowBean(Denominacionmoneda denominacionmoneda,
+			Integer cantidad) {
+		this.denominacionmoneda = denominacionmoneda;
+		this.cantidad = cantidad;
+	}
+
+	public Moneda getSubtotal() {
+		Moneda result = new Moneda();
+		result = denominacionmoneda.getValor().multiply(cantidad);
+		return result;
+	}
+
+	public Denominacionmoneda getDenominacionmoneda() {
+		return denominacionmoneda;
+	}
+
+	public void setDenominacionmoneda(Denominacionmoneda denominacionmoneda) {
+		this.denominacionmoneda = denominacionmoneda;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+}
