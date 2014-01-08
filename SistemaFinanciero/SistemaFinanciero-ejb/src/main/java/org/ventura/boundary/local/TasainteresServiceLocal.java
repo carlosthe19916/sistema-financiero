b5ef48ccbc05870b4f3ab5f6c199495ef4adc866
Jhon <Jhon@Flores>
2014-01-08 15:56:46 -0500

    123

--- SistemaFinanciero/SistemaFinanciero-ejb/src/main/java/org/ventura/boundary/local/TasainteresServiceLocal.java
+++ SistemaFinanciero/SistemaFinanciero-ejb/src/main/java/org/ventura/boundary/local/TasainteresServiceLocal.java
@@ -1,14 +1,21 @@
 package org.ventura.boundary.local;
 
+import java.math.BigDecimal;
+
 import javax.ejb.Local;
 
 import org.ventura.boundary.remote.TasainteresServiceRemote;
 import org.ventura.entity.schema.caja.TasaInteresTipoCambio;
 import org.ventura.entity.tasas.Tiposervicio;
 import org.ventura.entity.tasas.Tipotasa;
+import org.ventura.util.maestro.TipotasaCuentasPersonalesType;
 
 @Local
 public interface TasainteresServiceLocal extends TasainteresServiceRemote{
 	
 	public TasaInteresTipoCambio getTasainteres(Tiposervicio tiposervicio, Tipotasa tipotasa, Double monto) throws Exception;
+	
+	public BigDecimal getTasainteresCuentapersonal(TipotasaCuentasPersonalesType cuentasPersonalesType,BigDecimal monto) throws Exception;
+	
+	
 }
