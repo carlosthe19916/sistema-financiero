3172ca6561ec3d9a83a803f123a92e1bd986d7ae
carlosthe19916@gmail.com <eA4QU8hx6PE2>
2014-01-10 17:38:12 -0500

    5

--- SistemaFinanciero/SistemaFinanciero-ejb/src/main/java/org/ventura/boundary/local/TasainteresServiceLocal.java
+++ SistemaFinanciero/SistemaFinanciero-ejb/src/main/java/org/ventura/boundary/local/TasainteresServiceLocal.java
@@ -5,7 +5,9 @@ import java.math.BigDecimal;
 import javax.ejb.Local;
 
 import org.ventura.boundary.remote.TasainteresServiceRemote;
+import org.ventura.entity.schema.caja.Moneda;
 import org.ventura.entity.schema.caja.TasaInteresTipoCambio;
+import org.ventura.entity.schema.maestro.Tipomoneda;
 import org.ventura.entity.tasas.Tiposervicio;
 import org.ventura.entity.tasas.Tipotasa;
 import org.ventura.util.maestro.TipotasaCuentasPersonalesType;
@@ -17,5 +19,12 @@ public interface TasainteresServiceLocal extends TasainteresServiceRemote{
 	
 	public BigDecimal getTasainteresCuentapersonal(TipotasaCuentasPersonalesType cuentasPersonalesType,BigDecimal monto) throws Exception;
 	
+	public BigDecimal getTea(Tipomoneda tipomoneda, Integer periodo,BigDecimal monto) throws Exception;
+	
+	public BigDecimal getTrea(Tipomoneda tipomoneda, Integer periodo,BigDecimal monto) throws Exception;
+	
+	public BigDecimal getTasainteresCuentaahorro(Tipomoneda tipomoneda, BigDecimal monto) throws Exception;
+	
+	public BigDecimal getTasainteresCuentacorriente(Tipomoneda tipomoneda, BigDecimal monto) throws Exception;
 	
 }
