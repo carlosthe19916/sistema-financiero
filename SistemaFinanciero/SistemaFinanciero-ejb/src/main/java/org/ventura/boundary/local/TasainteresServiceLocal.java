822c93b5d9fa614ba55e31b64d2c0019626ed6db
carlosthe19916@gmail.com <eA4QU8hx6PE2>
2014-01-10 17:46:27 -0500

    3

--- SistemaFinanciero/SistemaFinanciero-ejb/src/main/java/org/ventura/boundary/local/TasainteresServiceLocal.java
+++ SistemaFinanciero/SistemaFinanciero-ejb/src/main/java/org/ventura/boundary/local/TasainteresServiceLocal.java
@@ -5,12 +5,10 @@ import java.math.BigDecimal;
 import javax.ejb.Local;
 
 import org.ventura.boundary.remote.TasainteresServiceRemote;
-<<<<<<< HEAD
+
 import org.ventura.entity.schema.caja.Moneda;
 import org.ventura.entity.schema.caja.TasaInteresTipoCambio;
 import org.ventura.entity.schema.maestro.Tipomoneda;
-=======
->>>>>>> branch 'master' of https://code.google.com/p/sistema-financiero/
 import org.ventura.entity.tasas.Tiposervicio;
 import org.ventura.entity.tasas.Tipotasa;
 import org.ventura.tipodato.TasaCambio;
@@ -24,7 +22,6 @@ public interface TasainteresServiceLocal extends TasainteresServiceRemote{
 	
 	public BigDecimal getTasainteresCuentapersonal(TipotasaCuentasPersonalesType cuentasPersonalesType,BigDecimal monto) throws Exception;
 	
-<<<<<<< HEAD
 	public BigDecimal getTea(Tipomoneda tipomoneda, Integer periodo,BigDecimal monto) throws Exception;
 	
 	public BigDecimal getTrea(Tipomoneda tipomoneda, Integer periodo,BigDecimal monto) throws Exception;
@@ -33,7 +30,8 @@ public interface TasainteresServiceLocal extends TasainteresServiceRemote{
 	
 	public BigDecimal getTasainteresCuentacorriente(Tipomoneda tipomoneda, BigDecimal monto) throws Exception;
 	
-=======
 	public TasaCambio getTipoCambioCompraVenta(TipoCambioCompraVentaType compraVentaType, BigDecimal monto) throws Exception;
->>>>>>> branch 'master' of https://code.google.com/p/sistema-financiero/
+
+	public TasaCambio getTipoCambioCompraVenta(TipoCambioCompraVentaType compraVentaType, BigDecimal monto) throws Exception;
+
 }
