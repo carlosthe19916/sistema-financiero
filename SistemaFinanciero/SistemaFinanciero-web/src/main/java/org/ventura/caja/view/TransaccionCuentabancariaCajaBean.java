af3683252b96eb8ded798905340c4178435bb47c
Jhon <Jhon@Flores>
2014-01-29 09:00:02 -0500

    2

--- SistemaFinanciero/SistemaFinanciero-web/src/main/java/org/ventura/caja/view/TransaccionCuentabancariaCajaBean.java
+++ SistemaFinanciero/SistemaFinanciero-web/src/main/java/org/ventura/caja/view/TransaccionCuentabancariaCajaBean.java
@@ -295,6 +295,21 @@ public class TransaccionCuentabancariaCajaBean implements Serializable {
 			monto = new BigDecimal("0.00");
 		}
 	}
+	
+	public String returnNumOperacion(){
+		String numOperacion = null;
+		Integer numeroop = vouchercajaView.getNumeroOperacion();
+		if (numeroop > 0 && numeroop < 10) {
+			numOperacion = "000" + numeroop;
+		}if (numeroop >= 10 && numeroop < 100) {
+			numOperacion = "00" + numeroop;
+		}if (numeroop >= 100 && numeroop < 1000) {
+			numOperacion = "0" + numeroop;
+		}if (numeroop >= 1000) {
+			numOperacion = "" + numeroop;
+		}
+		return numOperacion;
+	}
 
 	public void changeTipomoneda(ValueChangeEvent event) {	
 		Integer key = (Integer) event.getNewValue();
