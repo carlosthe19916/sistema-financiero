e8ac19cd783e12e51140eecd6839b358d8c4d7c2
SOFTGREEN <SOFTGREEN@Jhon-PC>
2014-04-22 15:26:19 -0500

    variables del sistema

---
+++ SistemaFinanciero/SistemaFinanciero-ejb/src/main/java/org/ventura/entity/schema/maestro/VariableSistema.java
@@ -0,0 +1,76 @@
+package org.ventura.entity.schema.maestro;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+
+/**
+ * The persistent class for the variable_sistema database table.
+ * 
+ */
+@Entity
+@Table(name="variable_sistema")
+@NamedQuery(name="VariableSistema.findAll", query="SELECT v FROM VariableSistema v")
+public class VariableSistema implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(unique=true, nullable=false)
+	private Integer idvariablesistema;
+
+	@Column(nullable=false, length=100)
+	private String denominacion;
+
+	@Column(length=200)
+	private String descripcion;
+
+	private Integer idtipomoneda;
+
+	@Column(nullable=false, precision=18, scale=3)
+	private BigDecimal valor;
+
+	public VariableSistema() {
+	}
+
+	public Integer getIdvariablesistema() {
+		return this.idvariablesistema;
+	}
+
+	public void setIdvariablesistema(Integer idvariablesistema) {
+		this.idvariablesistema = idvariablesistema;
+	}
+
+	public String getDenominacion() {
+		return this.denominacion;
+	}
+
+	public void setDenominacion(String denominacion) {
+		this.denominacion = denominacion;
+	}
+
+	public String getDescripcion() {
+		return this.descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Integer getIdtipomoneda() {
+		return this.idtipomoneda;
+	}
+
+	public void setIdtipomoneda(Integer idtipomoneda) {
+		this.idtipomoneda = idtipomoneda;
+	}
+
+	public BigDecimal getValor() {
+		return this.valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+}
---
+++ SistemaFinanciero/SistemaFinanciero-ejb/src/main/java/org/ventura/util/maestro/VariableSistemaType.java
@@ -0,0 +1,11 @@
+package org.ventura.util.maestro;
+
+public enum VariableSistemaType {
+
+	MONTO_APORTE_MENOR_EDAD,
+	
+	MONTO_APORTE_MAYOR_EDAD,
+	
+	MONTO_MAXIMO_TRANSACCION
+
+}
